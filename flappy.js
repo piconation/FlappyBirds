85563cd271d0a7371a51461d05be8d5bb526959b
mattpowell <piconation@yahoo.com>
2016-06-14 10:32:35 -0600

    6.14.16

--- flappy.js
+++ flappy.js
@@ -2,37 +2,140 @@
  * Created by mattpowell on 6/13/16.
  */
 
-function Dictionary() {
-    this.add = add;
-    this.datastore = {};
-    this.find = find;
-    this.remove = remove;
-    this.showAll = showAll;
+var fish;
+var coral;
+var currentState;
+var width;
+var height;
+
+var states = {
+    Splash: 0,
+    Game: 1,
+    Score: 2
+};
+
+function main() {
+    windowSetup();
+    canvasSetup();
+
+    currentState = states.Splash; //Game begins at the splash screen.
+
+    document.body.appendChild(canvas); //append the canvas we've created tot he body element in our html doc.
+
+    fish = new Fish();
+    //coral = new CoralCollection();
+
+    loadGraphics();
 }
 
-function add(key, value) {
-    this.datastore[key] = value;
+function windowSetup() {
+    //retrieve the width and height of the window.
+    width = window.innerWidth;
+    height = window.innerHeight;
+    var inputEvent = "touchstart";
+    //set the width and height if we are on a display with a width > 500px (i.e. - a desktop or tablet)
+    if (width >= 500) {
+        width = 380;
+        height = 430;
+        inputEvent = "mousedown";
+    }
+    //create a listener on the input event.
+    document.addEventListener(inputEvent, onpress);
 }
 
-function find(key) {
-    return this.datastore[key];
+function canvasSetup() {
+    canvas = document.createElement("canvas");
+    canvas.style.border = "15px solid #blue";
+    canvas.width = width;
+    canvas.height = height;
+    renderingContext = canvas.getContext("2d");
 }
 
-function remove(key) {
-    delete this.datastore[key];
-}
+function onpress(evt) {
+    switch (currentState) {
 
-function showAll() {
-    for(var key in this.datastore) {
-        console.log(key + "--> " + this.datastore[key]);
+        case states.Splash: // Start the game and update the fish velocity.
+            currentState = states.Game;
+            fish.jump();
+            break;
+
+        case states.Game: // The game is in progress. Update fish velocity.
+            fish.jump();
+            break;
+
+        case states.Score: // Change from score to splash state if event within okButton bounding box
+            // Get event position
+            var mouseX = evt.offsetX, mouseY = evt.offsetY;
+
+            if (mouseX == null || mouseY == null) {
+                mouseX = evt.touches[0].clientX;
+                mouseY = evt.touches[0].clientY;
+            }
+
+            // Check if within the okButton
+            if (okButton.x < mouseX && mouseX < okButton.x + okButton.width &&
+                okButton.y < mouseY && mouseY < okButton.y + okButton.height
+            ) {
+                //console.log('click');
+                corals.reset();
+                currentState = states.Splash;
+                score = 0;
+            }
+            break;
     }
 }
 
-var phonebook = new Dictionary();
+function Fish() {
+    this.x = 140;
+    this.y = 0;
 
-phonebook.add("Mike", "123-5555");
-phonebook.add("Julie", "123-4444");
-phonebook.add("Fred", "123-6666");
-console.log("Mike's number is:" + phonebook.find("Mike"));
-phonebook.remove("Mike");
-phonebook.showAll()
+    this.draw = function (renderingContext) {
+        
+    }
+}
+
+function loadGraphics() {
+    //initiates graphics and ok button
+    var img = new Image();
+    img.src = "sheet.png";
+    img.onload = function () {
+        initiateSprites(this);
+        renderingContext.fillStyle = backgroundSprite.color;
+        renderingContext.fillRect(0, 0, width, height);
+        backgroundSprite.draw(renderingContext, 0, height - backgroundSprite.height);
+        backgroundSprite.draw(renderingContext, backgroundSprite.width, height - backgroundSprite.height); //sets background color
+        fishSprite[0].draw(renderingContext, 5, 5, 142, 50);
+
+        /* Turned off temporarily
+            okButton = {
+            x: (width - okButtonSprite.width) / 2,
+            y: height - 200,
+            width: okButtonSprite.width,
+            height: okButtonSprite.height
+        };
+
+        gameLoop();*/
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
